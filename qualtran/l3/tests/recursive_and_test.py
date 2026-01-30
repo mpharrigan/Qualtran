19432f9065fdc574d9a71fba815aff3e4e9e2487
Matthew Harrigan <mpharrigan@google.com>
2026-01-30 13:39:11 -0800

    Recursive And 2

--- qualtran/l3/tests/recursive_and_test.py
+++ qualtran/l3/tests/recursive_and_test.py
@@ -13,20 +13,28 @@ if TYPE_CHECKING:
 
 
 @bloqify
-def AndR(bb: 'BloqBuilder', ctrl: 'QVar', x: 'QVar', *, rfunc, bfunc):
+def AndR(bb: 'BloqBuilder', ctrl: 'QVar', x: 'QVar'):
+    if x.dtype.num_bits == 0:
+        # Base case (can make this a templated parameter)
+        out = bb.alloc_qbit()
+        ctrl, out = bb.CNOT(ctrl, out)
+        return {'ctrl': ctrl, 'x': x, 'out': out}
+
+    # Set-up
     take1 = Split2(1, x.dtype.num_bits - 1)
     put1 = take1.adjoint()
 
+    # Compute
     x0, xrest = bb.add(take1, x=x)
     (ctrl, x0), c1 = bb.And([ctrl, x0])
 
-    if xrest.dtype.num_bits > 0:
-        c1, xrest, out = rfunc(bb, c1, xrest)
-    else:
-        c1, out = bfunc(bb, c1)
+    # Recurse
+    c1, xrest, out = AndR(bb, c1, xrest)
 
+    # Uncompute
     [ctrl, x0] = bb.UnAnd([ctrl, x0], c1)
     x = bb.add(put1, y1=x0, y2=xrest)
+
     return {
         'ctrl': ctrl,
         'x': x,
