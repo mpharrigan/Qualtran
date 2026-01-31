a62d0b8ae9c745c7348fd37daebdb3a3c3cb7b55
Matthew Harrigan <mpharrigan@google.com>
2026-01-30 16:13:45 -0800

    Recursive select

--- qualtran/l3/tests/qrom_test.py
+++ qualtran/l3/tests/qrom_test.py
@@ -12,14 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict, Tuple
 
 import numpy as np
+import attrs
+import sympy
 
 import qualtran as qlt
 import qualtran.dtype as qdt
 from qualtran.bloqs.arithmetic import XorK
+from qualtran.bloqs.bookkeeping import Split2
 from qualtran.l3 import bloqify
+from qualtran.symbolics import SymbolicInt
 
 if TYPE_CHECKING:
     from qualtran import BloqBuilder, QVar, QVarT
@@ -29,7 +33,7 @@ def cxor_k_bloq(k, bitsize=8):
     return XorK(qdt.QUInt(bitsize), k=k).controlled()
 
 
-OPS = np.array([cxor_k_bloq(k=100 + i) for i in range(2**3)]).reshape((2,) * 3)
+OPS = np.array([cxor_k_bloq(k=100 + i) for i in range(2 ** 3)]).reshape((2,) * 3)
 
 
 @bloqify
@@ -70,3 +74,45 @@ def cselect(bb, ctrl: 'QVar', selects: 'QVarT', system: 'QVarT', address=()):
 
 def test_cselect():
     cs = cselect.make(qlt.Signature.build(ctrl=1, selects=qdt.QBit()[3], system=8))
+
+
+@attrs.frozen
+class SymbolicSelect(qlt.Bloq):
+    n: SymbolicInt
+    m: SymbolicInt
+    address: Tuple[int, ...] = ()
+
+    @property
+    def signature(self) -> 'qlt.Signature':
+        return qlt.Signature.build(ctrl=1, selects=qdt.QAny(self.m), system=qdt.QAny(self.n))
+
+    def build_composite_bloq(self, bb: 'qlt.BloqBuilder', ctrl, selects, system):
+        if self.m == 0:
+            k = sympy.IndexedBase('k')
+            ctrl, system = bb.add(XorK(dtype=qdt.QUInt(self.n), k=k[self.address]).controlled(),
+                                  ctrl=ctrl, x=system)
+            return {'ctrl': ctrl, 'selects': selects, 'system': system}
+
+        # Set-up
+        take1 = Split2(1, self.m - 1)
+        put1 = take1.adjoint()
+
+        select, subselects = bb.add(take1, x=selects)
+        [ctrl, select], active = bb.And([ctrl, select], cv1=1, cv2=0)
+
+        active, subselects, system = bb.add(
+            SymbolicSelect(self.n, self.m - 1, address=self.address + (0,)),
+            ctrl=active, selects=subselects, system=system)
+
+        # Flip `active`
+        ctrl, active = bb.CNOT(ctrl, active)
+
+        active, subselects, system = bb.add(
+            SymbolicSelect(self.n, self.m - 1, address=self.address + (1,)),
+            ctrl=active, selects=subselects, system=system
+        )
+
+        [ctrl, select] = bb.UnAnd([ctrl, select], active, cv1=1, cv2=1)
+
+        selects = bb.add(put1, y1=select, y2=subselects)
+        return {'ctrl': ctrl, 'selects': selects, 'system': system}
--- qualtran/l3/tests/recursive_and_test.py
+++ qualtran/l3/tests/recursive_and_test.py
@@ -1,5 +1,6 @@
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Dict
 
+import attrs
 import numpy as np
 
 import qualtran as qlt
@@ -7,6 +8,7 @@ import qualtran.dtype as qdt
 from qualtran.l3 import bloqify
 
 from qualtran.bloqs.bookkeeping import Split2, Join2
+from qualtran.symbolics import SymbolicInt
 
 if TYPE_CHECKING:
     from qualtran import BloqBuilder, QVar
@@ -40,3 +42,39 @@ def AndR(bb: 'BloqBuilder', ctrl: 'QVar', x: 'QVar'):
         'x': x,
         'out': out
     }
+
+
+@attrs.frozen
+class SymbolicMultiAnd(qlt.Bloq):
+    n: SymbolicInt
+
+    @property
+    def signature(self) -> 'qlt.Signature':
+        return qlt.Signature.build(ctrl=1, x=qdt.QAny(self.n), out=(None, qdt.QBit()))
+
+    def build_composite_bloq(self, bb: 'BloqBuilder', ctrl, x):
+        if self.n == 0:
+            out = bb.alloc_qbit()
+            ctrl, out = bb.CNOT(ctrl, out)
+            return {'ctrl': ctrl, 'x': x, 'out': out}
+
+        # Set-up
+        take1 = Split2(1, self.n - 1)
+        put1 = take1.adjoint()
+
+        # Compute
+        x0, xrest = bb.add(take1, x=x)
+        (ctrl, x0), c1 = bb.And([ctrl, x0])
+
+        # Recurse
+        c1, xrest, out = bb.add(SymbolicMultiAnd(self.n - 1), ctrl=c1, x=xrest)
+
+        # Uncompute
+        [ctrl, x0] = bb.UnAnd([ctrl, x0], c1)
+        x = bb.add(put1, y1=x0, y2=xrest)
+
+        return {
+            'ctrl': ctrl,
+            'x': x,
+            'out': out
+        }
